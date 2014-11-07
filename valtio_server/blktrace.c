63a5a66a4c0bdda64fca381058d1ff2fcbae474c
Jungho Bang <me@bangtoven.com>
2014-11-07 14:58:02 +0900

    restore blktrace

--- valtio_server/blktrace.c
+++ valtio_server/blktrace.c
@@ -2692,154 +2692,58 @@ void writeToClient(struct blk_io_trace * t){
 
 int blk_main(int argc, char *argv[])
 {
+	int ret = 0;
 
-	int listenfd; /* listening socket */
-	//int connfd; /* connection socket */
-	int portno; /* port to listen on */
-	int clientlen; /* byte size of client's address */
-	struct sockaddr_in serveraddr; /* server's addr */
-	struct sockaddr_in clientaddr; /* client addr */
-	struct hostent *hostp; /* client host info */
-	char *hostaddrp; /* dotted decimal host addr string */
-	int optval; /* flag value for setsockopt */
-
-	/* check command line args */
-	/*  if (argc != 2) {
-	fprintf(stderr, "usage: %s <port>\n", argv[0]);
-	exit(1);
-	}*/
-	portno = net_port;
-
-	/* socket: create a socket */
-	listenfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (listenfd < 0) 
-		error("ERROR opening socket");
-
-	/* setsockopt: Handy debugging trick that lets 
-	* us rerun the server immediately after we kill it; 
-	* otherwise we have to wait about 20 secs. 
-	* Eliminates "ERROR on binding: Address already in use" error. 
-	*/
-	optval = 1;
-	setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, 
-	(const void *)&optval , sizeof(int));
-
-	/* build the server's internet address */
-	bzero((char *) &serveraddr, sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET; /* we are using the Internet */
-	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); /* accept reqs to any IP addr */
-	serveraddr.sin_port = htons((unsigned short)portno); /* port to listen on */
-
-	/* bind: associate the listening socket with a port */
-	if (bind(listenfd, (struct sockaddr *) &serveraddr, 
-	sizeof(serveraddr)) < 0) 
-		error("ERROR on binding");
-
-	/* listen: make it a listening socket ready to accept connection requests */
-	if (listen(listenfd, 5) < 0) /* allow 5 requests to queue up */ 
-		error("ERROR on listen");
-
-	/* main loop: wait for a connection request, echo input line, 
-	then close connection. */
-	clientlen = sizeof(clientaddr);
-	while (1) {
-
-		/* accept: wait for a connection request */
-		connfd = accept(listenfd, (struct sockaddr *) &clientaddr, (socklen_t *)&clientlen);
-		if (connfd < 0) 
-			error("ERROR on accept");
-    
-		/* gethostbyaddr: determine who sent the message */
-		hostaddrp = inet_ntoa(clientaddr.sin_addr);
-		printf("client addr: %s\n",hostaddrp);
-		hostp = gethostbyname(hostaddrp);//, clientlen, AF_INET);
-		if (hostp == NULL)
-			error("ERROR on gethostbyaddr");
-		if (hostaddrp == NULL)
-			error("ERROR on inet_ntoa\n");
-		printf("server established connection with %s (%s)\n", 
-		hostp->h_name, hostaddrp);
-    
-
-		/* read: read input string from the client */
-		/*   bzero(buf, BUFSIZE);
-		n = read(connfd, buf, BUFSIZE);
-		if (n < 0) 
-		error("ERROR reading from socket");
-		printf("server received %d bytes: %s", n, buf);
-		*/    
-		/* write: echo the input string back to the client */
-		/*   n = write(connfd, buf, strlen(buf));
-		if (n < 0) 
-		error("ERROR writing to socket");
-		*/
-
-
-
-		//    close(connfd);
-  
-
-		//original code from here on.
-
-
-
-		int ret = 0;
-
-		setlocale(LC_NUMERIC, "en_US");
-		pagesize = getpagesize();
-		ncpus = sysconf(_SC_NPROCESSORS_CONF);
-		if (ncpus < 0) {
-			fprintf(stderr, "sysconf(_SC_NPROCESSORS_CONF) failed %d/%s\n",
-			errno, strerror(errno));
-			ret = 1;
-			goto out;
-		} else if (handle_args(argc, argv)) {
-			ret = 1;
-			goto out;
-		}
-
-		if (ndevs > 1 && output_name && strcmp(output_name, "-") != 0) {
-			fprintf(stderr, "-o not supported with multiple devices\n");
-			ret = 1;
-			goto out;
-		}
-
-		signal(SIGINT, handle_sigint);
-		signal(SIGHUP, handle_sigint);
-		signal(SIGTERM, handle_sigint);
-		signal(SIGALRM, handle_sigint);
-		signal(SIGPIPE, SIG_IGN);
-
-		if (kill_running_trace) {
-			struct devpath *dpp;
-			struct list_head *p;
-
-			__list_for_each(p, &devpaths) {
-				dpp = list_entry(p, struct devpath, head);
-				if (__stop_trace(dpp->fd)) {
-					fprintf(stderr,
-					"BLKTRACETEARDOWN %s failed: %d/%s\n",
-					dpp->path, errno, strerror(errno));
-				}
-			}
-		} else if (net_mode == Net_server) {
-			if (output_name) {
-				fprintf(stderr, "-o ignored in server mode\n");
-				output_name = NULL;
-			}
-			ret = net_server();
-		} else
-			ret = run_tracers();
-
-		out:
-		if (pfp)
-			fclose(pfp);
-		rel_devpaths();
-
-		close(connfd);
-		return ret;
-
+	setlocale(LC_NUMERIC, "en_US");
+	pagesize = getpagesize();
+	ncpus = sysconf(_SC_NPROCESSORS_CONF);
+	if (ncpus < 0) {
+		fprintf(stderr, "sysconf(_SC_NPROCESSORS_CONF) failed %d/%s\n",
+		errno, strerror(errno));
+		ret = 1;
+		goto out;
+	} else if (handle_args(argc, argv)) {
+		ret = 1;
+		goto out;
 	}
 
-	
+	if (ndevs > 1 && output_name && strcmp(output_name, "-") != 0) {
+		fprintf(stderr, "-o not supported with multiple devices\n");
+		ret = 1;
+		goto out;
+	}
+
+	signal(SIGINT, handle_sigint);
+	signal(SIGHUP, handle_sigint);
+	signal(SIGTERM, handle_sigint);
+	signal(SIGALRM, handle_sigint);
+	signal(SIGPIPE, SIG_IGN);
+
+	if (kill_running_trace) {
+		struct devpath *dpp;
+		struct list_head *p;
+
+		__list_for_each(p, &devpaths) {
+			dpp = list_entry(p, struct devpath, head);
+			if (__stop_trace(dpp->fd)) {
+				fprintf(stderr,
+				"BLKTRACETEARDOWN %s failed: %d/%s\n",
+				dpp->path, errno, strerror(errno));
+			}
+		}
+	} else if (net_mode == Net_server) {
+		if (output_name) {
+			fprintf(stderr, "-o ignored in server mode\n");
+			output_name = NULL;
+		}
+		ret = net_server();
+	} else
+		ret = run_tracers();
+
+out:
+	if (pfp)
+		fclose(pfp);
+	rel_devpaths();
+
+	return ret;
 }
