6d5ed983ec80df8d60b38aabf42f608bc7dd1108
minitu <minitu@empas.com>
2014-11-07 18:08:51 +0900

    Added connection & environment setup windows and system tray icon

---
+++ ValtioClient/ValtioClient/EnvSetup.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ValtioClient
+{
+    /// <summary>
+    /// Interaction logic for EnvSetup.xaml
+    /// </summary>
+    public partial class EnvSetup : Window
+    {
+        String device;
+        String _traceLength;
+        String _timeWindow;
+        String _blockUnit;
+        List<String> items;
+
+        public EnvSetup()
+        {
+            InitializeComponent();
+
+            items = new List<String>();
+            items.Add("Element 1");
+            items.Add("Element 2");
+            items.Add("Element 3");
+            deviceList.ItemsSource = items;
+        }
+
+        private void traceBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _traceLength = traceLength.Text;
+            _timeWindow = timeWindow.Text;
+            _blockUnit = blockUnit.Text;
+            MessageBox.Show("Device: " + device + "\nTrace length: " + _traceLength + "\nTime window: " + _timeWindow + "\nBlock unit: " + _blockUnit);
+            Window TracingIcon = new TracingIcon();
+            this.WindowStyle = WindowStyle.SingleBorderWindow;
+            this.WindowState = WindowState.Minimized;
+            TracingIcon.Show();
+        }
+
+        private void deviceList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            device = deviceList.SelectedItem.ToString();
+            MessageBox.Show(device);
+        }
+    }
+}
--- ValtioClient/ValtioClient/TracingIcon.xaml.cs
+++ ValtioClient/ValtioClient/TracingIcon.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ValtioClient
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for TracingIcon.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TracingIcon : Window
     {
-        public MainWindow()
+        public TracingIcon()
         {
             InitializeComponent();
         }
