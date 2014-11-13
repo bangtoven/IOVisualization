a790f3b3a2695866004e777c32a20a26cd55c17f
minitu <minitu@empas.com>
2014-11-13 19:56:10 +0900

    Modified preference window & added elapsed time and progress percentage to tray icon

--- ValtioClient/ValtioClient/TracingIcon.xaml.cs
+++ ValtioClient/ValtioClient/TracingIcon.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace ValtioClient
 {
@@ -19,9 +21,44 @@ namespace ValtioClient
     /// </summary>
     public partial class TracingIcon : Window
     {
+        private TaskbarIcon tb;
+        private Stopwatch sw;
+        private String timeElapsed;
+        private int donePercent;
+
         public TracingIcon()
         {
             InitializeComponent();
+
+            tb = ValtioNotifyIcon;
+
+            tb.ToolTipText = "Tracing...";
+            sw = new Stopwatch();
+            sw.Start();
+        }
+
+        private void stopTrace_Click(object sender, RoutedEventArgs e)
+        {
+            Window MainWindow = new MainWindow();
+            MainWindow.Show();
+            tb.Dispose();
+            this.Close();
+        }
+
+        private void quitTrace_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ValtioNotifyIcon_TrayToolTipOpen(object sender, RoutedEventArgs e)
+        {
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            timeElapsed = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            double frac = ts.TotalSeconds / GlobalPref.getTraceLength();
+            donePercent = (int)(frac * 100);
+            tb.ToolTipText = "Tracing...\n" + "Elapsed: " + timeElapsed + "\nProgress: " + donePercent + "%";
+            sw.Start();
         }
     }
 }
